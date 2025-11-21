43f173a7e13ecbbf865d6b5355285e5ff44f207f
jsp <jsp@rien.com>
2025-11-21 17:27:20 +0100

    Finish index page

---
+++ AIDE_EXTERIEURE.md
@@ -0,0 +1,3 @@
+ChatGPT :   - how to take the system style like darkmode on a website
+            - how to see if a var exist in php
+            - how to rollback to the previous page in php
--- index.php
+++ index.php
@@ -1,11 +1,32 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome</title>
+
+    <link rel="stylesheet" href="./assets/style/stylesheet_global.css">
+    <link rel="stylesheet" href="./assets/style/stylesheet_index.css">
+
+    <title>Stickman game</title>
 </head>
 <body>
-    
+    <header>
+        <h1>Bienvenue</h1>
+        <p>Ce site est fortement inspirer du jeu <a href="https://www.crazygames.fr/jeu/fleeing-the-complex"><b>Fleeing the Complex</b></a> de la serie de jeu en ligne <b>Henry Stickmin</b>.</p>
+    </header>
+
+    <main>
+        <form id="chooseName" action="./page/node.php" method="post">
+            <fieldset>
+                <label for="userName">Comment t'appele-tu étranger ?</label>
+                <input type="text" name="userName" id="userNameInput">
+            </fieldset>
+        </form>
+    </main>
+
+    <?php
+        $isIndex = true;
+        require "./templates/_footer.phtml"
+    ?>
 </body>
 </html>
